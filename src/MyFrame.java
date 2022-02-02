ec2f41cfe138994ba9fc6ae1ef8f3f7c40b1c030
Dudjfy <chirin.dima@gmail.com>
2022-02-02 22:00:31 +0100

    made buttons appear in the right order

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -9,15 +9,15 @@ public class MyFrame extends JFrame{
         int buttonStartY = 100;
         int buttonSizeX = 50;
         int buttonSizeY = 50;
-        int rows = 4;
+        int rows = 3;
         int columns = 3;
 
-        for (int y=0; y < columns; y++){
-            for (int x=0; x < rows; x++){
-                int num = y * rows + x + 1;
+        for (int y=0; y < rows; y++){
+            for (int x=0; x < columns; x++){
+                int num = y * columns + x + 1;
                 JButton button = new JButton();
                 button.setBounds(buttonStartX + buttonSizeX * x,
-                        buttonStartY + buttonSizeY * y,
+                        buttonStartY + buttonSizeY * rows - buttonSizeY * y,
                         buttonSizeX,
                         buttonSizeY);
                 button.addActionListener(e -> System.out.println("Button " + num));
